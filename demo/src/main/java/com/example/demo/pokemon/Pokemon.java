58c6d8feeb3031ad985b6d008e099a35adae9ddd
Michał Łabuda <mlabuda544@gmail.com>
2024-01-20 12:32:22 +0100

    Change javax to jakarta

--- demo/src/main/java/com/example/demo/pokemon/Pokemon.java
+++ demo/src/main/java/com/example/demo/pokemon/Pokemon.java
@@ -1,8 +1,8 @@
 package com.example.demo.pokemon;
 
 import lombok.Data;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 @Data
